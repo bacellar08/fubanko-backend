f30d397843b5bf47154b94530d8316b606e5b318
Alexandre <alexandre.bacellar@gmail.com>
2024-07-12 04:38:09 -0300

    feat: boilerplate config

---
+++ src/main/java/io/github/fubanko/fubanko/FubankoApplication.java
@@ -0,0 +1,13 @@
+package io.github.fubanko.fubanko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FubankoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FubankoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fubanko/fubanko/FubankoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fubanko.fubanko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FubankoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
