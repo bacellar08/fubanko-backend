096a5d40ef35bfed03def8ea88d2498119341868
Alexandre <alexandre.bacellar@gmail.com>
2024-09-26 15:28:51 -0300

    feat: changed JWT token validation to use custom filter and single secret key.

--- src/main/java/io/github/fubanko/fubanko/config/SecurityConfig.java
+++ src/main/java/io/github/fubanko/fubanko/config/SecurityConfig.java
@@ -1,13 +1,9 @@
 package io.github.fubanko.fubanko.config;
 
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,28 +11,22 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${jwt.security.private-key}")
-    private RSAPrivateKey privateKey;
-    @Value("${jwt.security.public-key}")
-    private RSAPublicKey publicKey;
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, SecurityFilter securityFilter) throws Exception {
         http
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
@@ -55,26 +45,9 @@ public class SecurityConfig {
                         .requestMatchers("/users/**").authenticated()
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
-
-    }
-
-    @Bean
-    JwtEncoder jwtEncoder() {
-
-        var jwKey = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
-        var jwKeySet = new ImmutableJWKSet<>(new JWKSet(jwKey));
-
-        return new NimbusJwtEncoder(jwKeySet);
-
     }
 
     @Bean
--- src/main/java/io/github/fubanko/fubanko/service/JwtService.java
+++ src/main/java/io/github/fubanko/fubanko/service/JwtService.java
@@ -1,9 +1,11 @@
 package io.github.fubanko.fubanko.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -11,11 +13,9 @@ import java.time.Instant;
 @Service
 public class JwtService {
 
-    private final JwtEncoder jwtEncoder;
+    @Value("${jwt.security.secret-key}")
+    private String secretKey;
 
-    public JwtService(JwtEncoder jwtEncoder) {
-        this.jwtEncoder = jwtEncoder;
-    }
 
     public String generateToken(Authentication authentication) {
 
@@ -24,16 +24,37 @@ public class JwtService {
 
         var scopes = authentication.getAuthorities().toString();
 
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("fubanko")
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiration))
-                .subject(authentication.getName())
-                .claim("scopes", scopes)
-                .build();
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
-        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+            return JWT.create()
+                    .withIssuer("fubanko")
+                    .withSubject(authentication.getName())
+                    .withIssuedAt(now)
+                    .withExpiresAt(now.plusSeconds(expiration))
+                    .withClaim("scopes", scopes)
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Could not generate token", exception);
+        }
     }
+
+    public String validateToken(String token) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            return JWT.require(algorithm)
+                    .withIssuer("fubanko")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception) {
+            throw new RuntimeException("Invalid Token", exception);
+        }
+    }
+
+
 
 
 }
